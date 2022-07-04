ce4c65e692627ea3c3f47e62d78f1ff9f12980d6
murali0101 <muralimv0101@gmail.com>
2022-07-04 18:50:43 +0530

    Update SlideShow.jsx

--- src/components/home/SlideShow.jsx
+++ src/components/home/SlideShow.jsx
@@ -32,6 +32,7 @@ export const SlideShow = () => {
     };
 
     useEffect(() => {
+        
         let timerId = setInterval(() => {
             setIndex((prev) => prev === arr.length - 1 ? 0 : prev + 1);
         }, 6000);
@@ -39,18 +40,18 @@ export const SlideShow = () => {
         return () => {
             clearInterval(timerId);
         }
-    }, [arr.length]);
+    }, []);
 
     return (
         <Box className="slide shadow" backgroundImage={arr[index]} backgroundRepeat={'no-repeat'} backgroundSize={'cover'} h={['300px', '400px', '500px', '700px', '900px']}>
             <Center px={'15px'} h={['300px', '400px', '500px', '700px', '900px']}>
                 <Flex w={'100%'} justifyContent={'space-between'}>
                     <Center>
-                        <SlideIcon icon={AiFillCaretLeft} onClick={handleLeftIndex} value={-1} />
+                        <SlideIcon icon={AiFillCaretLeft} onClick={handleLeftIndex}  />
                     </Center>
                     <SlideText path={'/products'} title={section[index]} />
                     <Center>
-                        <SlideIcon icon={AiFillCaretRight} onClick={handleRightIndex} value={1} />
+                        <SlideIcon icon={AiFillCaretRight} onClick={handleRightIndex}  />
                     </Center>
                 </Flex>
             </Center>
